9acaf4c36004e71d1eb5e947ffeb6055d2efd755
Abhimanyu Sharma <abhimanyu254a@gmail.com>
2026-06-23 13:35:13 +0530

    Add JWT token generation methods and update dependencies

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,6 +56,31 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password);
 } 
+user.Schema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email:this.email,
+            username:this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+user.Schema,methods.generateRefreshToken = function(){
+        return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 export const User = mongoose.model("User", userSchema); 
